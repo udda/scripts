0f07ec16582363e15de94202fecb7daedc43d4e6
Mario Cianciolo <udda@users.noreply.github.com>
2016-03-05 02:26:13 +0100

    Initial commit

---
+++ debug-script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Copyright (C) 2011-2016 Mario Cianciolo <mr.udda@gmail.com>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+# This script runs a Bash script printing each line while it is executed
+
+if [ "$#" = "0" ]; then
+	echo "USAGE: debug-script.sh <script>"
+	exit 1
+fi
+exec env PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }' bash -x $*
